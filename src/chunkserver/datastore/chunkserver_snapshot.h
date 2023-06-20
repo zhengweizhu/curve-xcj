4692d7561a0d78a33921b1f312470e2a611c3f10
thincat75 <thincat.china@gmail.com>
2023-06-19 23:13:16 -0700

    add local clone chunkserver logical add DLOG(INFO) --> LOG(INFO) add more LOG for READ change LOG(INFO) --> DLOG(INFO) add readlock for the function DivideObjInfoByIndex

--- src/chunkserver/datastore/chunkserver_snapshot.h
+++ src/chunkserver/datastore/chunkserver_snapshot.h
@@ -204,6 +204,8 @@ class CSSnapshot {
     std::shared_ptr<FilePool> chunkFilePool_;
     // datastore internal statistical indicators
     std::shared_ptr<DataStoreMetric> metric_;
+
+    uint64_t cloneNo_;
 };
 
 class CSSnapshots {
@@ -220,6 +222,11 @@ class CSSnapshots {
     CSErrorCode Move(SequenceNum from, SequenceNum to);
     CSErrorCode Merge(SequenceNum from, SequenceNum to);
     virtual ~CSSnapshots();
+
+    
+    bool DivideSnapshotObjInfoByIndex (SequenceNum sn, std::vector<BitRange>& range, 
+                                    std::vector<BitRange>& notInRanges, 
+                                    std::vector<ObjectInfo>& objInfos);
 
  private:
     std::vector<CSSnapshot*>::iterator find(SequenceNum sn);
